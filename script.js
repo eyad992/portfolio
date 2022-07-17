00c55597b8c4d4863d130314b2d9a8ef1cc4ce4a
eyad992 <eyadgarro410@gmail.com>
2022-07-17 21:06:20 +0200

    changes

--- script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $("a").click(function(){
-    var pageId = $(this).attr("data-page");
-    $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
-  });
-});
